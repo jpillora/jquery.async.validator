946da26bfc97c209ee29c82280f0a2451eace08a
Jaime Pillora <jaime@gmail.com>
2012-12-03 17:49:50 +1100

    demos nearly working

--- demo/demo.js
+++ demo/demo.js
@@ -4,93 +4,25 @@ require.config({
   }
 });
 
-define([,
+define([
   'util/log',
   'jquery',
   '../dist/jquery.async.validator',
-  'css!../demo/demo',
   'underscore',
   'lib/prettify',
   'lib/bootstrap.min'],
   function(log) {
 
-    // require(['css!framework/css/bootstrap.cerulean.min'], function() {
-    //   $('.loading-cover').stop().delay(2000).fadeOut('fast');
-    // });
+    function setCode(container, pre) {
 
-    var demos = [
-      'basic',
-      // 'regular-expressions',
-      // 'groups',
-      // 'extending',
-      // 'user-defined'
-    ];
-
-    //
-    function titlise(str, sep, join) {
-      var p, part, parts, _i, _len;
-      if (sep == null) 
-        sep = ' ';
-      if (join == null) 
-        join = ' ';
-      parts = str.split(sep);
-      for (p = _i = 0, _len = parts.length; _i < _len; p = ++_i) {
-        part = parts[p];
-        parts[p] = part.charAt(0).toUpperCase() + part.substr(1).toLowerCase();
-      }
-      return parts.join(join);
-    }
-
-    //
-    window.initScripts = {};
-
-    var demoTemplate = _.template($("#demoTemplate").html());
-    _.each(demos, buildDemo);
-
-    function buildDemo(name) {
-      var demoContainer = $(demoTemplate({
-        name: name,
-        title: titlise(name)
-      }));
-      $("#demos").append(demoContainer);
-    }
-
-    function setupDemo() {
-      var container = $(this);
-      if(container.hasClass('ready')) return;
-
-      var name = container.data('name');
-
-      $.get('demo/demos/'+name+'.html', 
-        function(data) {
-          var demo = $(data),
-              script = demo.siblings('script[data-demo]'),
-              html = demo.siblings('div[data-demo]');
-
-          setCode(script.html(), container.find('pre.script'));
-          setCode(html.html(), container.find('pre.html'));
-
-          container.find('.demo-container').html(demo);
-
-          window.initScripts[name](html);
-
-          container.togglers();
-          container.slideDown('slow').addClass('ready');
-        }
-      );
-    }
-
-    function setCode(content, pre) {
-
-      if(!content) return;
-      content = content.replace('demoContainer.find','$');
-
+      if(!container.length === 0 || pre.length === 0) return;
+      var content = container.html();
       var lines = content.split('\n'), indentation;
 
       content = "";
       for (var i = 0, l = lines.length; i < l; ++i) {
         var line = lines[i];
-        if(i == 0 || i == l-1) continue;
+        if(i === 0 || i === l-1) continue;
 
         if(indentation === undefined) {
           var m = line.match(/^(\ *)/);
@@ -102,28 +34,6 @@ define([,
       pre.append(prettyPrintOne(encode(content)));
     }
 
-    $(document).ready(function() {
-
-      //open user demo on load
-      var hash = window.location.hash.substr(1);
-      if(hash) 
-      $('#'+hash).on('hidden', function() { 
-        $(this).collapse("show").off('hidden');
-      });
-
-      $('.collapse').collapse('hide').on('show', setupDemo);
-
-      $('.loading-cover').fadeOut('fast');
-
-      window.prettyPrint();
-
-      //intercept all form submissions
-      $("#demos").on("submit","form", function() {
-        console.log("All validations passed on form: " + $(this).attr('id') + " !");
-        return false;
-      });
-      
-    });
 
     //helpers
     function encode(value) {
@@ -139,13 +49,13 @@ define([,
             selector = btn.attr('data-toggle'),
             elem = container.find(selector);
 
-        if(elem.length == 0) return;
+        if(elem.length === 0) return;
 
         btn.click(function() {
           var visible = elem.is(':visible');
           
           var text = btn.html();
-          btn.html(text.replace(/hide|show/i, visible ? 'Show' : 'Hide'))
+          btn.html(text.replace(/hide|show/i, visible ? 'Show' : 'Hide'));
 
           if(visible)
             elem.slideUp();
@@ -154,8 +64,60 @@ define([,
         });
 
       });
+    };
+
+
+    function create(type) { return $("<"+type+"/>"); }
+
+    function setupNav() {
+
+      var header = _.template('<li class="nav-header"><%= title %></li>'),
+          anchor = _.template('<li><a href="#<%= title %>"><%= title %></a></li>'),
+          navList = $("#nav-list");
+
+      $("[data-nav-heading]").each(function() {
+
+        navList.append(header({title: $(this).data('nav-heading') }));
+
+        $(this).find("[data-nav-anchor]").each(function() {
+          var title = $(this).data("nav-anchor");
+          $(this).attr('id', title);
+          navList.append(anchor( {title: title}));
+        });
+      });
     }
 
+    function setupCodeSnippets() {
+      $(".demo").each(function() {
+
+
+        var demo = $(this),
+            htmlDemo = demo.find("div[data-html]"),
+            htmlPre = demo.find("pre[data-html]"),
+            scriptDemo = demo.find("script[data-script]"),
+            scriptPre = demo.find("pre[data-script]");
+
+        setCode(htmlDemo, htmlPre);
+        setCode(scriptDemo, scriptPre);
+
+      });
+    }
+
+    $(document).ready(function() {
+
+      setupNav();
+      setupCodeSnippets();
+
+      $('.loading-cover').fadeOut('fast');
+
+      window.prettyPrint();
+
+      //intercept all form submissions
+      $("#demos").on("submit","form", function() {
+        return false;
+      });
+      
+    });
 
   }
 );
